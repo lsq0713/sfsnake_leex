6a766d898fa22d73e6e597d37f811c51f0f99970
lessen <1841795355@qq.com>
2024-06-10 22:29:42 +0800

    my original version todo

---
+++ CMakeFiles/main.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/lee/workspace/OOP/pj/sfSnake-master/src/Fruit.cpp" "CMakeFiles/main.dir/src/Fruit.cpp.o" "gcc" "CMakeFiles/main.dir/src/Fruit.cpp.o.d"
+  "/home/lee/workspace/OOP/pj/sfSnake-master/src/Game.cpp" "CMakeFiles/main.dir/src/Game.cpp.o" "gcc" "CMakeFiles/main.dir/src/Game.cpp.o.d"
+  "/home/lee/workspace/OOP/pj/sfSnake-master/src/GameOverScreen.cpp" "CMakeFiles/main.dir/src/GameOverScreen.cpp.o" "gcc" "CMakeFiles/main.dir/src/GameOverScreen.cpp.o.d"
+  "/home/lee/workspace/OOP/pj/sfSnake-master/src/GameScreen.cpp" "CMakeFiles/main.dir/src/GameScreen.cpp.o" "gcc" "CMakeFiles/main.dir/src/GameScreen.cpp.o.d"
+  "/home/lee/workspace/OOP/pj/sfSnake-master/src/MenuScreen.cpp" "CMakeFiles/main.dir/src/MenuScreen.cpp.o" "gcc" "CMakeFiles/main.dir/src/MenuScreen.cpp.o.d"
+  "/home/lee/workspace/OOP/pj/sfSnake-master/src/Screen.cpp" "CMakeFiles/main.dir/src/Screen.cpp.o" "gcc" "CMakeFiles/main.dir/src/Screen.cpp.o.d"
+  "/home/lee/workspace/OOP/pj/sfSnake-master/src/Snake.cpp" "CMakeFiles/main.dir/src/Snake.cpp.o" "gcc" "CMakeFiles/main.dir/src/Snake.cpp.o.d"
+  "/home/lee/workspace/OOP/pj/sfSnake-master/src/SnakeNode.cpp" "CMakeFiles/main.dir/src/SnakeNode.cpp.o" "gcc" "CMakeFiles/main.dir/src/SnakeNode.cpp.o.d"
+  "/home/lee/workspace/OOP/pj/sfSnake-master/src/main.cpp" "CMakeFiles/main.dir/src/main.cpp.o" "gcc" "CMakeFiles/main.dir/src/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
